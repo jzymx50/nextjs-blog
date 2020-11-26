350ecb182384a1476071dba1176eed21a270efbd
Ziyang Jiang <jzymx50@gmail.com>
2020-11-25 19:14:47 -0500

    config

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const execSync = require("child_process").execSync;
+
+const lastCommitCommand = "git rev-parse HEAD";
+
+module.exports = {
+    async generateBuildId() {
+        return execSync(lastCommitCommand).toString().trim();
+    },
+};
